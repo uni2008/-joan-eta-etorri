c1fc0bda0204d7b7249adb68c766840ed2746206
uni2008 <93262463+uni2008@users.noreply.github.com>
2022-03-23 14:22:37 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
+    basic.pause(1000)
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 40)
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 40)
+    basic.pause(1000)
+    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
+    basic.pause(1000)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+})
+input.onButtonPressed(Button.B, function () {
+    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 70)
+    basic.pause(1000)
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 70)
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 70)
+    basic.pause(1000)
+    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 0)
+    basic.pause(1000)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+})
+basic.forever(function () {
+    DFRobotMaqueenPlus.I2CInit()
+})
